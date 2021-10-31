4248302c765ef1698f0e4917bc20dbc96582156b
AverkinaNastya <50995302+AverkinaNastya@users.noreply.github.com>
2021-10-31 18:38:43 +0300

    Лабораторная работа 28 пример 1

---
+++ ЛР28/LR28_example_1/LR28_example_1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#pragma comment(lib, "netapi32.lib")
+#ifndef UNICODE
+#define UNICODE
+#endif
+#include <stdio.h>
+#include <windows.h>
+#include <lm.h>
+#include <lmaccess.h>
+#include <iostream>
+BOOL DelUser(PWSTR pszName) {
+	NET_API_STATUS nStatus;
+	nStatus = NetUserDel(NULL, pszName);
+	return (nStatus == NERR_Success);
+}
+int main() {
+	if (!DelUser(L"ExpUser"))
+		printf("A system error has occurred");
+	return 0;
+}
