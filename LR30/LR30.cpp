3d0b2056af5bd5ee2dff5bed64a4650cc4f04e55
AverkinaNastya <50995302+AverkinaNastya@users.noreply.github.com>
2021-11-05 18:50:25 +0300

    Лабораторная работа 30

---
+++ LR30/LR30.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, ".1251");
+	float a, b, c;
+	cout << "\na = "; cin >> a;
+	cout << "\nb = "; cin >> b;
+	cout << "\nc = "; cin >> c;
+	if (a <= 0 || b <= 0 || c <= 0) {
+		cout << "\n Длины сторон должны быть положительными";
+	}
+	else if ((a + b) < c || (a + c) < b || (b + c) < a) {
+		cout << "\n Такой треугольник не существует";
+	}
+	else {
+		if (a == b && a == c)
+		{
+			cout << "\n Треугольник равносторонний";
+		}
+		else cout << "\n Треугольник не равносторонний.";
+	}
+}
